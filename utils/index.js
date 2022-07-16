b6ca2e23d92ecf9a04a4d025616d11c58f99cd37
Brian Schaaf <brian@Brians-Air-2.hsd1.ga.comcast.net>
2022-07-15 20:35:18 -0400

    create utils index

--- utils/index.js
+++ utils/index.js
@@ -1,6 +1,13 @@
+const term = require('')
+
 module.exports = {
   commandType: (input) => {
+    if (input === '') {
+      return 'Please enter a valid command';
+    }
     const command = input.split(' ')
+
+    if
     console.log('command:', command)
   },
 }
