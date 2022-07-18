61f05be7d8bfa85088534581ed024627d7b0271c
Brian Schaaf <bschaaf1017@gmail.com>
2022-07-17 22:44:46 -0400

    refactored add warehouse to use hash table

--- services/warehouse.js
+++ services/warehouse.js
@@ -7,39 +7,45 @@ const {
 
 module.exports = {
   addWarehouse: (input) => {
-    input = input.split(' ')
+    input = input.split(' ');
+
     if (input.length <= 2) {
       terminal.red('\nYou must provide a Warhouse number');
       return;
     }
 
-    const warehouseNum = parseInt(input[2]);
+    const warehouseNum = input[2];
     const stockLimit = input[3] ? parseInt(input[3]) : null;
     const file = readJsonfile();
     const { warehouses } = file;
     let isSameNum = false;
 
-    for (let i = 0; i < warehouses.length; i++) {
-      if (warehouses[i].warehouseNum === warehouseNum) {
-        isSameNum = true;
-        break;
-      }
-    }
-
-    if (isSameNum) {
+    if (warehouses[warehouseNum]) {
       terminal.red(`\nA warehouse with number: ${warehouseNum} already exists.`);
       return;
     }
 
+    // for (let i = 0; i < warehouses.length; i++) {
+    //   if (warehouses[i].warehouseNum === warehouseNum) {
+    //     isSameNum = true;
+    //     break;
+    //   }
+    // }
+
+    // if (isSameNum) {
+    //   terminal.red(`\nA warehouse with number: ${warehouseNum} already exists.`);
+    //   return;
+    // }
+
     const newFile = {
       ...file,
-      warehouses: [
+      warehouses: {
         ... warehouses,
-        {
+        [warehouseNum]: {
           warehouseNum,
           stockLimit
         }
-      ]
+      }
     }
 
     writeToJsonFile(newFile);
